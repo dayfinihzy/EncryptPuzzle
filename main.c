1968ebf1a17d41bcda80f18777cb37c2879e6938
dayfinihzy <44896247+dayfinihzy@users.noreply.github.com>
2018-12-05 13:34:35 +0800

    第一次作业
    第一次作业

---
+++ main.c
@@ -0,0 +1,34 @@
+//
+//  main.c
+//  iOS Club
+//
+//  Created by 侯之洋 on 2018/11/16.
+//  Copyright © 2018年 侯之洋. All rights reserved.
+//
+
+#include <stdio.h>
+int main()
+{
+    int n,i,k;
+    char s[100];
+    printf("请输入向前推几位字母：\n");
+    scanf("%d",&n);
+    printf("请输入小写字母，输入其他（除空格外）结束读入\n");
+    
+    for(i=0;i<100;i++)
+    {
+         scanf("%c",&s[i]);
+        if((s[i]<'a'||s[i]>'z')&&(s[i]!=' '))
+            break;
+    }
+    k=i;
+    for(i=0;i<=k;i++)
+    {
+        if(s[i]>='a'&&s[i]<=122-n)
+            s[i]=s[i]+n;
+         else if(s[i]>=123-n&&s[i]<='z')
+             s[i]=s[i]-(26-n);
+        printf("%c",s[i]);
+    }
+    return 0;
+}
